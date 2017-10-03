139b28f389e8ae8dcb2720a91f3136039fdbabce
Yanxiang Yang <leoyyx2009@gmail.com>
2017-10-03 03:52:22 +0000

    add flash and error message, create show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root "pages#home"
+  get "about", to: 'pages#about'
+  
+  resources :articles
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,8 +13,6 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  root "pages#home"
-  get "about", to: 'pages#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
