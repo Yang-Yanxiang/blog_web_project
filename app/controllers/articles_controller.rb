9e98aca4a454fb76100e06ef22273150dfbbbcdd
Yanxiang Yang <leoyyx2009@gmail.com>
2017-10-03 04:37:47 +0000

    add index, add back button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end
+  
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -15,6 +21,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to article_path(@article)
+    else 
+      render 'edit'
+    end
+  end
+  
+  
+  
   
   private
   def article_params
