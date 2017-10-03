b2a762df75f79015cd1bb7a875eaaa56da326545
Yanxiang Yang <leoyyx2009@gmail.com>
2017-10-03 20:10:17 +0000

    add delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,13 @@ class ArticlesController < ApplicationController
     end
   end
   
-  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    flash[:notice] = "Article delete successfully"
+    redirect_to articles_path
+  end
   
   
   private
